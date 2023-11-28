6ca01975ba7f926880e80fe72758f889a80ebe1d
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-28 16:32:55 +0600

    Added delete news

--- src/pages/Dashboard/MyNews/MyNews.jsx
+++ src/pages/Dashboard/MyNews/MyNews.jsx
@@ -2,24 +2,42 @@ import { useContext } from "react";
 import useNews from "../../../hooks/useNews";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import { Link } from "react-router-dom";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import {toast} from "react-hot-toast"
 
 const MyNews = () => {
-  const [news] = useNews();
+  const [news, refetch] = useNews();
+  const axiosPublic = useAxiosPublic()
   const { user } = useContext(AuthContext);
   const email = user?.email;
 
   const myNews = news.filter((item) => item.email === email);
   console.log(myNews);
 
+  const handleDeleteItem = id => {
+    
+            axiosPublic.delete(`/news/${id}`)
+            .then(res => {
+                console.log(res.data);
+                if(res.data.deletedCount > 0){
+                  refetch();
+                  toast.success('your news article deleted')
+                }
+              })
+}
+
+
+  const {loading} = useContext(AuthContext)
+  if(loading){
+    return <span>loading...</span>
+}
+
   return (
-    <div>
+    <div className="max-w-5xl mx-auto">
       <h1 className="py-10 items-center text-center font-bold text-3xl underline">My News: {myNews.length}</h1>
       {myNews.map((item) => (
         
-        <div key={item._id}
-        data-aos="fade-up"
-        className=" w-[800px] rounded-md shadow-md dark:bg-gray-900 "
-      >
+        <div key={item._id} className="w-[800px] mx-auto" >
          <div className="flex justify-between font-bold px-6 my-3">
             <p>Published by: {item.publisher}</p>
             <p>Date: {item.date}</p>
@@ -38,10 +56,10 @@ const MyNews = () => {
            <Link to={`/news/update/${item._id}`}>
            <button className="btn btn-sm bg-yellow-500 hover:bg-yellow-400">Update</button>
            </Link>
-            <button className="btn btn-sm bg-red-500 hover:bg-red-600">Delete</button>
+            <button onClick={()=>handleDeleteItem(item._id)}  className="btn btn-sm bg-red-500 hover:bg-red-600">Delete</button>
           </div>
         </div>
-      </div>
+       </div>
       ))}
     </div>
   );
